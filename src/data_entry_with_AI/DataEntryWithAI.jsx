63372819fcdd9d3b6eb027cd75cebfe6402643b3
vuongoctien <vuongoctien@gmail.com>
2025-06-23 16:26:34 +0700

    kết thúc dự án

--- src/data_entry_with_AI/DataEntryWithAI.jsx
+++ src/data_entry_with_AI/DataEntryWithAI.jsx
@@ -11,6 +11,8 @@ import { Worker } from '@react-pdf-viewer/core'; // install this library
 // CSS
 import './DataEntryWithAI.css'
 import axios from 'axios';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export const DataEntryWithAI = () => {
     //preview
@@ -27,6 +29,15 @@ export const DataEntryWithAI = () => {
     const [text, setText] = useState('');
     const [loading, setLoading] = useState(false);
     const [textShow, setTextShow] = useState('');
+
+    // 
+    const [don_vi_hanh_chinh, set_don_vi_hanh_chinh] = useState('');
+    const [loai_van_ban, set_loai_van_ban] = useState('');
+    const [ngay_ban_hanh, set_ngay_ban_hanh] = useState('');
+    const [nguoi_ky, set_nguoi_ky] = useState('');
+    const [so_va_ky_hieu, set_so_va_ky_hieu] = useState('');
+    const [trich_yeu_noi_dung, set_trich_yeu_noi_dung] = useState('');
+
 
 
     const handleUploadPDF = (e) => {
@@ -79,9 +90,40 @@ export const DataEntryWithAI = () => {
                         "Content-Type": "multipart/form-data"
                     }
                 });
+                console.log('response.data', response.data)
                 setText(prev => {
                     const newText = [...prev]; // tạo bản sao mới của mảng
-                    newText[i] = response.data.text; // cập nhật phần tử i
+                    newText[i] = response.data.text_raw; // cập nhật phần tử i
+                    return newText; // trả về mảng mới để React biết cần re-render
+                });
+                set_don_vi_hanh_chinh(prev => {
+                    const newText = [...prev]; // tạo bản sao mới của mảng
+                    newText[i] = response.data.gpt_result.don_vi_hanh_chinh; // cập nhật phần tử i
+                    return newText; // trả về mảng mới để React biết cần re-render
+                });
+                set_loai_van_ban(prev => {
+                    const newText = [...prev]; // tạo bản sao mới của mảng
+                    newText[i] = response.data.gpt_result.loai_van_ban; // cập nhật phần tử i
+                    return newText; // trả về mảng mới để React biết cần re-render
+                });
+                set_ngay_ban_hanh(prev => {
+                    const newText = [...prev]; // tạo bản sao mới của mảng
+                    newText[i] = response.data.gpt_result.ngay_ban_hanh; // cập nhật phần tử i
+                    return newText; // trả về mảng mới để React biết cần re-render
+                });
+                set_nguoi_ky(prev => {
+                    const newText = [...prev]; // tạo bản sao mới của mảng
+                    newText[i] = response.data.gpt_result.nguoi_ky; // cập nhật phần tử i
+                    return newText; // trả về mảng mới để React biết cần re-render
+                });
+                set_so_va_ky_hieu(prev => {
+                    const newText = [...prev]; // tạo bản sao mới của mảng
+                    newText[i] = response.data.gpt_result.so_va_ky_hieu; // cập nhật phần tử i
+                    return newText; // trả về mảng mới để React biết cần re-render
+                });
+                set_trich_yeu_noi_dung(prev => {
+                    const newText = [...prev]; // tạo bản sao mới của mảng
+                    newText[i] = response.data.gpt_result.trich_yeu_noi_dung; // cập nhật phần tử i
                     return newText; // trả về mảng mới để React biết cần re-render
                 });
 
@@ -90,10 +132,28 @@ export const DataEntryWithAI = () => {
                 alert("Đã xảy ra lỗi khi xử lý file.");
             }
             setLoading(false);
-            console.log('tyle', text)
         }
 
     }
+
+    const prev = () => {
+        const newIndex = Math.max(0, indexSelectedFile - 1);
+        set_indexSelectedFile(newIndex);
+        chooseFile(arrFile, newIndex, (result) => {
+            set_pdfURL(result);
+        });
+        setTextShow(text[newIndex])
+    }
+
+    const next = () => {
+        const newIndex = Math.min(arrFile.length - 1, indexSelectedFile + 1);
+        set_indexSelectedFile(newIndex);
+        chooseFile(arrFile, newIndex, (result) => {
+            set_pdfURL(result);
+        });
+        setTextShow(text[newIndex])
+    }
+
     return (
         <div className=''>
             <div className='side-menu'>
@@ -108,14 +168,13 @@ export const DataEntryWithAI = () => {
                     {arrFile && arrFile.length > 0 &&
                         arrFile.map((item, index) => {
                             return (
-                                <li className='file_to_dom'
+                                <li key={index} className='file_to_dom'
                                     onClick={() => {
                                         chooseFile(arrFile, index, (result) => {
                                             set_indexSelectedFile(index);
                                             set_pdfURL(result);
                                         });
                                         setTextShow(text[index])
-                                        console.log('text', text[index])
                                     }}>
                                     {item.name}
                                 </li>)
@@ -125,25 +184,8 @@ export const DataEntryWithAI = () => {
             <div className='main'>
                 <div className='preview'>
                     <div className='prev-next'>
-                        <a className="previous" onClick={() => {
-                            const newIndex = Math.max(0, indexSelectedFile - 1);
-                            set_indexSelectedFile(newIndex);
-                            chooseFile(arrFile, newIndex, (result) => {
-                                set_pdfURL(result);
-                            });
-                            setTextShow(text[newIndex])
-                            console.log('text', text[newIndex])
-                        }}>&laquo; Prev</a>
-                        <a className="next" onClick={() => {
-                            const newIndex = Math.min(arrFile.length - 1, indexSelectedFile + 1);
-                            set_indexSelectedFile(newIndex);
-                            chooseFile(arrFile, newIndex, (result) => {
-                                set_pdfURL(result);
-                            });
-                            setTextShow(text[newIndex])
-                            console.log('text', text[newIndex])
-                        }}>Next &raquo;</a>
-
+                        <a className="previous" onClick={prev}>&laquo; Prev</a>
+                        <a className="next" onClick={next}>Next &raquo;</a>
                     </div>
                     <div className='pdf-container'>
                         {pdfURL && <><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
@@ -151,16 +193,39 @@ export const DataEntryWithAI = () => {
                         </Worker></>}
                         {!pdfURL && <>No pdf file selected</>}
                     </div>
+                    <div className='prev-next'>
+                        <a className="previous" onClick={prev}>&laquo; Prev</a>
+                        <a className="next" onClick={next}>Next &raquo;</a>
+                    </div>
                 </div>
                 <div className='entry'>
-                    {loading && <p>Đang xử lý...</p>}
-
-                    {textShow && (
+                    <div className="header">
+                        <a className="logo">Vương Ngọc Tiến</a>
+                        <div className="header-right">
+                            <a>About this website</a>
+                        </div>
+                    </div>
+                    {/* {textShow && (
                         <div style={{ marginTop: '2rem', whiteSpace: 'pre-wrap', background: '#f0f0f0', padding: '1rem' }}>
                             <h3>Kết quả trích xuất:</h3>
                             <p>{textShow}</p>
                         </div>
-                    )}
+                    )} */}
+                    {loading && <p>Đã xử lý {text.length} / {arrFile.length} <FontAwesomeIcon icon={faSpinner} spin size="1x" /></p>}
+                    <div style={{ marginTop: '2rem', whiteSpace: 'pre-wrap', background: '#f0f0f0', padding: '1rem' }}>
+                        {don_vi_hanh_chinh && (<p>Đơn vị hành chính: {don_vi_hanh_chinh[indexSelectedFile]}</p>)}
+
+                        {loai_van_ban && (<p>Loại văn bản {loai_van_ban[indexSelectedFile]}</p>)}
+
+                        {ngay_ban_hanh && (<p>Ngày ban hành: {ngay_ban_hanh[indexSelectedFile]}</p>)}
+
+                        {nguoi_ky && (<p>Người ký: {nguoi_ky[indexSelectedFile]}</p>)}
+
+                        {so_va_ky_hieu && (<p>Số và ký hiệu: {so_va_ky_hieu[indexSelectedFile]}</p>)}
+
+                        {trich_yeu_noi_dung && (<p>Trích yếu nội dung: {trich_yeu_noi_dung[indexSelectedFile]}</p>)}
+
+                    </div>
                 </div>
             </div>
 
